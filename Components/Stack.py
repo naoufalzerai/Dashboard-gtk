3f04b6a085f3eaf11739984c3371f0ba52027f7d
naoufal zerai <naoufal.zerai@gmail.com>
2022-05-04 14:01:18 -0500

    load module dynamically

--- Components/Stack.py
+++ Components/Stack.py
@@ -1,3 +1,5 @@
+import importlib
+
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -8,6 +10,11 @@ from gi.repository import Gtk, Gio
 def init_stack(stack: Gtk.Stack, config: list, builder: Gtk.Builder):
     for page in config:
         builder.add_from_file(f"./Plugins/{page}/{page}.ui")
+
+        spec = importlib.util.spec_from_file_location(page, f"./Plugins/{page}/{page}.py")
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
         window = builder.get_object(page)
         name = "label%s" % page
         title = "Page %s" % page
